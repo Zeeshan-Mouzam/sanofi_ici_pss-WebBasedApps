4c2eb0a68a5ad5ddb6249cf0cbe2f857ab1da907
Zeeshan-Mouzam <167408799+Zeeshan-Mouzam@users.noreply.github.com>
2025-12-03 18:06:26 +0530

    Change suggestedMax to max for x-axis scale

--- app.js
+++ app.js
@@ -367,7 +367,7 @@ function createSubcategoryChart() {
             scales: {
                 x: {
                     beginAtZero: true,
-					suggestedMax: 50
+					max: 50 
                 },
                 y: {
                     ticks: {
